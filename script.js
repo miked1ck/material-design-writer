95b7d39f1bfa936d172cf2f3f69944ba8fa53d38
Michael Dick <michael@m1k3.net>
2016-10-24 21:20:33 -0700

    Code cleanup

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-// window/doc
 var viewHeight = 300;
 var viewWidth = 500;
-
-// els
 var button = document.querySelector('.button');
 var layout = document.querySelector('.layout');
 var textarea = document.querySelector('.editor__textarea');
-
 button.addEventListener('click', function() {
   layout.classList.add('layout--writing');
   button.style.width = viewWidth;
-  setTimeout(function() {
-    textarea.focus();
-  }, 600);
-
-  setTimeout(function() {
-    button.style.height = viewHeight;
-  }, 350);
+  setTimeout(function() { textarea.focus(); }, 600);
+  setTimeout(function() { button.style.height = viewHeight; }, 350);
 });
